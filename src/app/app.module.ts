8ea9e454dabf82ae48cb4224a2094ec26dd6e19b
Cardo Kambla <kamblaca@gmail.com>
2017-10-01 23:28:35 +0300

    #10 #20 added translation service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,32 @@ import { AppComponent } from './app.component';
 import {CustomerSideHeaderComponent} from "./shared/header/header.component";
 import {UserFormComponent} from "./user-form/user-form.component";
 import {FormsModule} from "@angular/forms";
+import {TranslateModule, TranslateLoader} from "@ngx-translate/core";
+import {HttpClient, HttpHandler, HttpClientModule} from "@angular/common/http";
+import {TranslateHttpLoader} from "@ngx-translate/http-loader";
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerSideHeaderComponent,
     UserFormComponent
-
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
