2cfa2b6a7869311759524724a5bd72cb79d03f33
Jaagup Kuhi <jaagupku@gmail.com>
2017-10-11 20:12:32 +0300

    #16 initial businiess client changing without authentication
    PUT /businessClient/<id>  body: JSON

--- server/api/controllers/businessClientController.js
+++ server/api/controllers/businessClientController.js
@@ -18,8 +18,7 @@ exports.create_a_client = function(req, res) {
 exports.read_a_client = function(req, res) {
 	fs.readFile(clientsFilePath, function(err, data) {
 		var clients = JSON.parse(data);
-		var i;
-		for (i in clients) {
+		for (var i in clients) {
 			if (clients[i].id == req.params.clientId) {
 				res.writeHead(200, {'Content-Type': 'application/json'});
 				res.write(JSON.stringify(clients[i]));
@@ -35,7 +34,31 @@ exports.read_a_client = function(req, res) {
 };
 
 exports.update_a_client = function(req, res) {
-	res.end();
+	fs.readFile(clientsFilePath, function(err, data) {
+		var clients = JSON.parse(data);
+		for (var i in clients) {
+			if (clients[i].id == req.params.clientId) {
+				
+				for (var key in req.body) {
+					if (key === 'id')
+						continue;
+					clients[i][key] = req.body[key];
+				}
+				
+				fs.writeFile(clientsFilePath, JSON.stringify(clients), function (err) {
+					if (err) throw err;
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify(clients[i]));
+					res.end();
+				});
+				return; // Id found exit function
+			}
+		}
+		// Id not found
+		res.writeHead(404, {'Content-Type': 'application/json'});
+		res.write('{"error": "Id not found"}');
+		res.end();
+	});
 };
 
 exports.delete_a_client = function(req, res) {
