cff445e55a1d444c795768458b98f11c5d206317
Jaagup Kuhi <jaagupku@gmail.com>
2017-10-12 16:54:10 +0300

    #6

--- server/server.js
+++ server/server.js
@@ -14,13 +14,17 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
+app.get('/github-webhook', (req, res) => {
+  res.end();
+});
+
 var routes = require('./api/routes/businessClientRoutes');
 routes(app);
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
-  
+
 app.listen(port);
 
 console.log('RESTful API server started on: ' + port);
