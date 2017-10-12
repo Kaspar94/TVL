71e43d51de04d12ab9d004bead25e18dabfe3504
Jaagup Kuhi <jaagupku@gmail.com>
2017-10-12 16:28:00 +0300

    #16 deleting client by id
    DELETE /businessClient/<id>

--- server/api/controllers/businessClientController.js
+++ server/api/controllers/businessClientController.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var fs = require('fs');
+var helper = require('../helpers/fileHelper');
 var clientsFilePath = './api/data/clients.json';
 
 exports.list_all_clients = function(req, res) {
@@ -17,50 +18,64 @@ exports.create_a_client = function(req, res) {
 
 exports.read_a_client = function(req, res) {
 	fs.readFile(clientsFilePath, function(err, data) {
-		var clients = JSON.parse(data);
-		for (var i in clients) {
-			if (clients[i].id == req.params.clientId) {
-				res.writeHead(200, {'Content-Type': 'application/json'});
-				res.write(JSON.stringify(clients[i]));
-				res.end();
-				return; // Id found exit function
-			}
+		var client = helper.findOneById(JSON.parse(data), req.params.clientId);
+		if (client) {
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			res.write(JSON.stringify(client));
+			res.end();
+		} else { // Id not found
+			res.writeHead(404, {'Content-Type': 'application/json'});
+			res.write(JSON.stringify({'status': 'error', 'cause': 'id not found'}));
+			res.end();
 		}
-		// Id not found
-		res.writeHead(404, {'Content-Type': 'application/json'});
-		res.write('{"error": "Id not found"}');
-		res.end();
 	});
 };
 
 exports.update_a_client = function(req, res) {
 	fs.readFile(clientsFilePath, function(err, data) {
 		var clients = JSON.parse(data);
-		for (var i in clients) {
-			if (clients[i].id == req.params.clientId) {
-				
-				for (var key in req.body) {
-					if (key === 'id')
-						continue;
-					clients[i][key] = req.body[key];
-				}
-				
-				fs.writeFile(clientsFilePath, JSON.stringify(clients), function (err) {
-					if (err) throw err;
-					res.writeHead(200, {'Content-Type': 'application/json'});
-					res.write(JSON.stringify(clients[i]));
+		var updatedClient = helper.updateOneById(clients, req.params.clientId, req.body);
+
+		if (updatedClient) {
+			fs.writeFile(clientsFilePath, JSON.stringify(clients), function (err) {
+				if (err) {
+					res.writeHead(500, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify({'status': 'error', 'cause': 'failed to write to file'}));
 					res.end();
-				});
-				return; // Id found exit function
-			}
+				} else {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify(updatedClient));
+					res.end();
+				}
+			});
+		} else { // Id not found
+			res.writeHead(404, {'Content-Type': 'application/json'});
+			res.write(JSON.stringify({'status': 'error', 'cause': 'id not found'}));
+			res.end();
 		}
-		// Id not found
-		res.writeHead(404, {'Content-Type': 'application/json'});
-		res.write('{"error": "Id not found"}');
-		res.end();
+
 	});
 };
 
 exports.delete_a_client = function(req, res) {
-	res.end();
+	fs.readFile(clientsFilePath, function(err, data) {
+		var clients = JSON.parse(data);
+		if (helper.removeOneById(clients, req.params.clientId)) {
+			fs.writeFile(clientsFilePath, JSON.stringify(clients), function (err) {
+				if (err) {
+					res.writeHead(500, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify({'status': 'error', 'cause': 'failed to write to file'}));
+					res.end();
+				} else {
+					res.writeHead(200, {'Content-Type': 'application/json'});
+					res.write(JSON.stringify({'status': 'success'}));
+					res.end();
+				}
+			});
+		} else { // Id not found
+			res.writeHead(404, {'Content-Type': 'application/json'});
+			res.write(JSON.stringify({'status': 'error', 'cause': 'id not found'}));
+			res.end();
+		}
+	});
 };
---
+++ server/api/helpers/fileHelper.js
@@ -0,0 +1,51 @@
+'use strict';
+
+exports.findOneById = function (array, id) {
+  return exports.findOne(array, 'id', id);
+}
+
+exports.findOne = function (array, key, value) {
+  for (var i in array) {
+    if (array[i][key] == value)
+      return array[i];
+  }
+  return null;
+}
+
+exports.updateOneById = function (array, id, newValues) {
+  return exports.updateOne(array, 'id', id, newValues);
+}
+
+exports.updateOne = function (array, key, value, newValues) {
+  var elem = exports.findOne(array, key, value);
+  if (!elem)
+    return null;
+
+  for (var attribute in newValues) {
+    if (attribute === 'id' || !elem[attribute])
+      continue;
+    elem[attribute] = newValues[attribute];
+  }
+
+  return elem;
+}
+
+exports.removeOneById = function (array, id) {
+  return exports.removeOne(array, 'id', id);
+}
+
+exports.removeOne = function (array, key, value) {
+  var index;
+  for (var i in array) {
+    if (array[i][key] == value) {
+      index = i;
+      break;
+    }
+  }
+  if (index) {
+    array.splice(index, 1);
+    return true;
+  } else {
+    return false;
+  }
+}
