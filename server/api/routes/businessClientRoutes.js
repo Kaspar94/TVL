fc98307d752f1cd5f54bcb2304b9dceabe084ab1
Jaagup Kuhi <jaagupku@gmail.com>
2017-10-13 17:26:54 +0300

    authentication with basic auth #25

--- server/api/routes/businessClientRoutes.js
+++ server/api/routes/businessClientRoutes.js
@@ -1,16 +1,17 @@
 'use strict';
-module.exports = function(app) {
+
+module.exports = function(app, passport) {
 	var businessClient = require('../controllers/businessClientController');
 
 	app.route('/businessClient')
 		.get(businessClient.list_all_clients)
-		.post(businessClient.create_a_client);
+		.post(passport.authenticate('basic', { session: false }), businessClient.create_a_client);
 
 	app.route('/businessClient/where')
 		.get(businessClient.filter_clients);
 
 	app.route('/businessClient/:clientId(\\d+)')
 		.get(businessClient.read_a_client)
-		.put(businessClient.update_a_client)
-		.delete(businessClient.delete_a_client);
+		.put(passport.authenticate('basic', { session: false }), businessClient.update_a_client)
+		.delete(passport.authenticate('basic', { session: false }), businessClient.delete_a_client);
 };
--- server/server.js
+++ server/server.js
@@ -2,30 +2,24 @@ var express = require('express'),
   app = express(),
   port = process.env.PORT || 80,
   bodyParser = require('body-parser'),
-  path = require('path');
+  path = require('path'),
+  passport = require('passport'),
+  BasicStrategy = require('passport-http').BasicStrategy,
+  User = require('./api/helpers/userHelper');
+
+passport.use(new BasicStrategy(
+  function(username, password, done) {
+    User.findOneByUsername(username, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!User.validatePassword(user, password)) { return done(null, false); }
+      return done(null, user);
+    });
+  }
+));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
-
-// Add headers
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
@@ -34,7 +28,7 @@ app.get('/', (req, res) => {
 });
 
 var routes = require('./api/routes/businessClientRoutes');
-routes(app);
+routes(app, passport);
 
 app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
