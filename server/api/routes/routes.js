2beccde6e5de7a4443237d68bfda1cfc3ba06174
Kaspar Kliimask <kaspar94@tdl.ee>
2017-10-28 14:24:46 +0300

    Building xml file. closes #37

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -3,7 +3,7 @@
 module.exports = function(app, passport) {
 	var businessClient = require('../controllers/businessClientController');
 	var auth = require('../controllers/authController');
-
+	var xmlController = require('../controllers/xmlController');
 	app.route('/login')
 		.get(passport.authenticate('basic', { session: false }), auth.login);
 
@@ -18,4 +18,6 @@ module.exports = function(app, passport) {
 		.get(businessClient.read_a_client)
 		.put(passport.authenticate('basic', { session: false }), businessClient.update_a_client)
 		.delete(passport.authenticate('basic', { session: false }), businessClient.delete_a_client);
+	app.route('/xml')
+		.post(xmlController.send_xml);
 };
