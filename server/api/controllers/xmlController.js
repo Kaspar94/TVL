d45e4723f1909ee0a61208adad89f05620d63ce3
Kaspar Kliimask <kaspar94@tdl.ee>
2017-11-07 20:55:58 +0200

    XML faili saatmine omniva APIle. #62

--- server/api/controllers/xmlController.js
+++ server/api/controllers/xmlController.js
@@ -1,9 +1,11 @@
 'use strict';
+var request = require('request');
 var fs = require('fs');
 var helper = require('../helpers/fileHelper');
 var encoding = 'utf8';
 var clientsFilePath = './api/data/clients.json';
 var returnsFilePath = './api/data/returnAddresses.json';
+var pwFilePath = './api/data/password.json';
 var randomstring = require("randomstring");
 
 exports.get_returns = function(req, res) {
@@ -25,10 +27,11 @@ exports.send_xml = function(req, res) {
 	fs.readFile(clientsFilePath, encoding, function(err, data) {			
 			var client = helper.findOneById(JSON.parse(data).data, req.body.business_id);
 			if (client) {
-				console.log(client.deliveryCountry);
+
 				fs.readFile(returnsFilePath, encoding, function(err, data) {
 					if(err) {
-						throw err;
+						res.status(404).json({'status': 'error', 'cause': 'could not load storage countries file returnAddresses.json'});
+						return;
 					}
 					var ret = null;
 					ret = helper.findOneByCountry(JSON.parse(data).data, client.deliveryCountry);
@@ -36,10 +39,11 @@ exports.send_xml = function(req, res) {
 						res.status(404).json({'status': 'error', 'cause': 'deliveryCountry ' + client.deliveryCountry + ' not found'});
 						return;
 					}
+					// create XML object
 					var obj = {
-					   "Envelope": {
-					      "Body": {
-						 "businessToClientMsgRequest": {
+					   "soapenv:Envelope": {
+					      "soapenv:Body": {
+						 "xsd:businessToClientMsgRequest": {
 						    "partner": "24510",
 						    "interchange": {
 						       "header": {
@@ -86,15 +90,45 @@ exports.send_xml = function(req, res) {
 					   }
 					}
 					if(req.body.client_number != null) {
-						obj.Envelope.Body.businessToClientMsgRequest.interchange.item_list.item.add_service['option code="GN"'] = "";
+						obj["soapenv:Envelope"]["soapenv:Body"]["xsd:businessToClientMsgRequest"].interchange.item_list.item.add_service['option code="GN"'] = "";
 					}
 					if(req.body.client_email != null) {
-						obj.Envelope.Body.businessToClientMsgRequest.interchange.item_list.item.add_service['option code="GM"'] = "";
+						obj["soapenv:Envelope"]["soapenv:Body"]["xsd:businessToClientMsgRequest"].interchange.item_list.item.add_service['option code="GM"'] = "";
 					}
 
 					var builder = require('xmlbuilder');
 					var xml = builder.create(obj);
-					res.send(xml.toString());
+					
+					// send xml to ominva API
+					fs.readFile(pwFilePath, encoding, function(err, data) {
+						if(err) {
+							res.status(404).json({'status': 'error', 'cause': 'could not find API password file'});
+							return;
+						}
+						var pw = JSON.parse(data);
+						console.log(pw);
+						if(pw != null) {
+							var options = {                 
+								method: 'POST',             
+								uri: 'https://edixml.post.ee/epmx/services/messagesService/',    
+								body: xml.toString(),                
+								headers: {    
+									'Content-Type': 'text/xml;charset=utf-8',           
+									'Authorization': 'Basic ' + new Buffer(pw.username+":"+pw.password).toString('base64')                  
+								}
+							};                                         
+							request(options, function(error, response, body) {  
+								if(response.statusCode == 200) {
+									res.send('{"status" : "success"}');
+								} else {
+									res.send('{"status" : "error", "cause" : "'+error+'"}');
+								}
+
+							});
+						} else {
+							res.status(404).json({'status': 'error', 'cause': 'password file data not found'});
+						}
+					});
 				});
 			} else { // Id not found
 				res.status(404).json({'status': 'error', 'cause': 'id not found'});
