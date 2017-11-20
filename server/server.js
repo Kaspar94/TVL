1a2d5c10bb541caffc9ea9d486917069d587e17a
Jaagup Kuhi <jaagupku@gmail.com>
2017-11-20 14:33:47 +0200

    #70

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('express'),
+  helmet = require('helmet'),
   app = express(),
   port = process.env.PORT || 80,
   bodyParser = require('body-parser'),
   path = require('path'),
+  config = require('config'),
   passport = require('passport'),
   BasicStrategy = require('passport-http').BasicStrategy,
   User = require('./api/helpers/userHelper');
@@ -17,6 +19,8 @@ passport.use(new BasicStrategy(
     });
   }
 ));
+
+app.use(helmet());
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -34,8 +38,21 @@ app.use(function(req, res) {
   res.status(404).send({url: req.originalUrl + ' not found'})
 });
 
-app.listen(port);
+if (config.util.getEnv('NODE_ENV') === 'production') {
+  var https = require('https');
+  var fs = require('fs');
+
+  var credetialsPaths = config.get('credentials');
+  console.log(credetialsPaths);
+
+  https.createServer({
+      key: fs.readFileSync(credetialsPaths.privateKeyPath),
+      cert: fs.readFileSync(credentialsPaths.certificatePath)
+  }, app).listen(55555);
+  console.log('RESTful API server started on: ' + port);
+} else {
+  app.listen(port);
+  console.log('RESTful API server started on: ' + port + "\nServer running in development mode. Set environment value 'NODE_ENV=production' to enable production mode.");
+}
 
 module.exports = app;
-
-console.log('RESTful API server started on: ' + port);
