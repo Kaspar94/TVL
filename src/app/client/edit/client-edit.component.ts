6217d29c6f67c25ce1495255261982b2046f9aee
Cardo Kambla <kamblaca@gmail.com>
2017-11-21 22:50:29 +0200

    #68

--- src/app/client/edit/client-edit.component.ts
+++ src/app/client/edit/client-edit.component.ts
@@ -1,11 +1,13 @@
 import {Component} from '@angular/core';
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
+import {NgbModal, NgbActiveModal, NgbModalOptions} from '@ng-bootstrap/ng-bootstrap';
 import {TranslateService} from '@ngx-translate/core';
 import {SharedService} from '../../shared/shared.service';
 import {BusinessClient} from '../../shared/shared.model';
-import {isNull} from 'util';
+import {isNull, isNullOrUndefined} from 'util';
 import {ClientService} from '../client.service';
 import {AlertService} from '../../shared/alert/alert.service';
+import {ConfirmDialogComponent} from '../confirm-dialog/confirm.dialog.component';
+import {LoginComponent} from '../../shared/login/login.component';
 
 @Component({
   selector: 'client-edit-data',
@@ -16,6 +18,7 @@ export class ClientEditComponent {
 
   tempClient: BusinessClient;
   client: BusinessClient;
+  modalOption: NgbModalOptions = {};
 
   constructor(translate: TranslateService,
               public activeModal: NgbActiveModal,
@@ -26,9 +29,17 @@ export class ClientEditComponent {
               private translateService: TranslateService) {
   }
 
-  remove(client: BusinessClient) {
-    this.clientService.removeClient(client);
+  remove() {
+    const dialog = (<ConfirmDialogComponent>this.modalService.open(ConfirmDialogComponent, this.modalOption).componentInstance);
+    dialog.setParentModal(this.activeModal);
+    dialog.setConfirmCallbackMethod(this.removeClient);
+    dialog.setTitle(this.translateService.instant('dialog.title.remove'));
+    dialog.setMessage(this.translateService.instant('dialog.message.remove'));
+  }
 
+  removeClient = () => {
+    this.clientService.removeClient(this.client);
+    this.alertService.success(this.translateService.instant('success.modifiedData'), this.translateService.instant('success.title'));
   }
 
   setClient(client: BusinessClient) {
@@ -38,10 +49,32 @@ export class ClientEditComponent {
   }
 
   close() {
+    this.revertChanges();
     this.activeModal.dismiss();
   }
 
+  revertChanges() {
+    if (!isNull(this.client.id)) {
+      this.client.axapta = this.tempClient.axapta;
+      this.client.name = this.tempClient.name;
+      this.client.street = this.tempClient.street;
+      this.client.city = this.tempClient.city;
+      this.client.postIndex = this.tempClient.postIndex;
+      this.client.country = this.tempClient.country;
+      this.client.deliveryCountry = this.tempClient.deliveryCountry;
+      this.client.serviceNumber = this.tempClient.serviceNumber;
+    }
+  }
+
   confirmChanges() {
+    const dialog = (<ConfirmDialogComponent>this.modalService.open(ConfirmDialogComponent, this.modalOption).componentInstance);
+    dialog.setParentModal(this.activeModal);
+    dialog.setConfirmCallbackMethod(this.confirmClient);
+    dialog.setTitle(this.translateService.instant(isNull(this.client.id) ? 'dialog.title.new' : 'dialog.title.edit'));
+    dialog.setMessage(this.translateService.instant(isNull(this.client.id) ? 'dialog.message.new' : 'dialog.message.edit'));
+  }
+
+  confirmClient = () => {
     if (this.clientEquals(this.tempClient, this.client)) {
       this.close();
     } else {
@@ -49,14 +82,14 @@ export class ClientEditComponent {
         this.clientService.createClient(this.client).subscribe((response) => {
           this.clientService.loadWithFilters();
           this.close();
-          this.alertService.success(this.translateService.instant('success.newData'), this.translateService.instant('success.title'))
-        },(err) => this.alertService.error(err))
+          this.alertService.success(this.translateService.instant('success.newData'), this.translateService.instant('success.title'));
+        }, (err) => this.alertService.error(err));
       } else {
         this.clientService.updateClient(this.client).subscribe((response) => {
           this.clientService.loadWithFilters();
           this.close();
-          this.alertService.success(this.translateService.instant('success.modifiedData'), this.translateService.instant('success.title'))
-        },(err) => this.alertService.error(err))
+          this.alertService.success(this.translateService.instant('success.modifiedData'), this.translateService.instant('success.title'));
+        }, (err) => this.alertService.error(err));
       }
     }
   }
@@ -79,5 +112,16 @@ export class ClientEditComponent {
 
   changeDeliveryCountry(wareHouse: string) {
       this.client.deliveryCountry = wareHouse;
+  }
+
+  isFormFilled() {
+    return !isNullOrUndefined(this.client.axapta) && this.client.axapta.length > 0
+      && !isNullOrUndefined(this.client.name) && this.client.name.length > 0
+      && !isNullOrUndefined(this.client.street) && this.client.street.length > 0
+      && !isNullOrUndefined(this.client.city) && this.client.city.length > 0
+      && !isNullOrUndefined(this.client.postIndex) && this.client.postIndex.length > 0
+      && !isNullOrUndefined(this.client.country) && this.client.country.length > 0
+      && !isNullOrUndefined(this.client.deliveryCountry) && this.client.deliveryCountry.length > 0
+      && !isNullOrUndefined(this.client.serviceNumber) && this.client.serviceNumber.length > 0;
   }
 }
